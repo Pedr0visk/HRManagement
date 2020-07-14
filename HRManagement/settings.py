0e66d0ff5268fcb5b307197f83885d2bb85ae415
Pedro Henrique <pedro357bm@gmail.com>
2020-07-14 07:52:53 -0300

    add departaments to project

--- HRManagement/settings.py
+++ HRManagement/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.companies',
-    'apps.employers'
+    'apps.employers',
+    'apps.departaments'
 ]
 
 MIDDLEWARE = [
